3d062d78bb0e5da7e8871125432ed2cfc8926a70
Steven Hornung <steven.hornung@icloud.com>
2013-06-26 23:20:08 -0400

    add second bootstrap user

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,13 +8,22 @@ class BootStrap {
 		def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
 		def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
 
-		def adminUser = Account.findByEmail('steven.hornung@icloud.com') ?: new Account(
+		def adminUser1 = Account.findByEmail('steven.hornung@icloud.com') ?: new Account(
 			email: 'steven.hornung@icloud.com',
-			password: 'vie11eicht',
+			password: 'password',
 			fullName: 'Steven Hornung').save(failOnError: true)
+			
+		if (!adminUser1.authorities.contains(adminRole)) {
+			AccountRole.create adminUser1, adminRole
+		}
+			
+		def adminUser2 = Account.findByEmail('brandon.shader@uky.edu') ?: new Account(
+			email: 'brandon.shader@uky.edu',
+			password: 'password',
+			fullName: 'BrandonShader').save(failOnError: true)
 
-		if (!adminUser.authorities.contains(adminRole)) {
-			AccountRole.create adminUser, adminRole
+		if (!adminUser2.authorities.contains(adminRole)) {
+			AccountRole.create adminUser2, adminRole
 		}
 
     }
