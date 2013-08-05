607f3b03ce0f94fb161f9018b8c6ce777042c481
Steven Hornung <steven.hornung@icloud.com>
2013-08-05 17:16:54 -0400

    move zip functionality to utility class for others to use

--- src/groovy/com/intercloud/cloudstore/DropboxCloudStore.groovy
+++ src/groovy/com/intercloud/cloudstore/DropboxCloudStore.groovy
@@ -21,6 +21,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import com.intercloud.*
+import com.intercloud.util.ZipUtilities
 
 class DropboxCloudStore implements CloudStoreInterface {
 	
@@ -248,12 +249,12 @@ class DropboxCloudStore implements CloudStoreInterface {
 		downloadFolderToPath(downloadedFolderPath, fileResource)
 		
 		log.debug "Zipping downloaded folder"
-		zipDownloadedFolder(downloadedFolderPath, zipFileName)
+		ZipUtilities.zipDownloadedFolder(downloadedFolderPath, zipFileName)
 		
 		String zipFileLocation = downloadedFolderPath.substring(0, downloadedFolderPath.lastIndexOf('/'))
-		byte[] zippedFolderBytes = getBytesFromZipFile(zipFileLocation, zipFileName)
+		byte[] zippedFolderBytes = ZipUtilities.getBytesFromZipFile(zipFileLocation, zipFileName)
 		
-		removeTempDownloadFolder(zipFileLocation)
+		ZipUtilities.removeTempDownloadFolder(zipFileLocation)
 		
 		return zippedFolderBytes
 	}
@@ -307,86 +308,5 @@ class DropboxCloudStore implements CloudStoreInterface {
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
 		DbxEntry entry = dropboxClient.getFile(fileResource.path, null, outputStream)
 		return outputStream.toByteArray()
-	}
-	
-	private void zipDownloadedFolder(String directory, String zipFileName) {
-		try {
-			String zipPath = directory.substring(0, directory.lastIndexOf('/'))
-			String fullZipPath = zipPath + "/" + zipFileName
-			FileOutputStream fos = new FileOutputStream(fullZipPath);
-			ZipOutputStream zos = new ZipOutputStream(fos)
-			File srcFile = new File(directory)
-			
-			packZip(zos, srcFile)
-			
-			zos.close()
-		}
-		catch(IOException) {
-			log.error "Error creating zip file: {}", IOException
-		}
-	}
-	
-	private void packZip(ZipOutputStream outputStream, File srcFile) {
-		File[] files = srcFile.listFiles()
-		for (File file : files) {
-			if (file.isDirectory()) {
-				zipDir(outputStream, file, "")
-			}
-			else {
-				zipFile(outputStream, file, "")
-			}
-		}
-	}
-	
-	private void zipDir(ZipOutputStream outputStream, File dir, String path) {
-		File[] files = dir.listFiles()
-		path = buildPath(path, dir.name)
-		
-		for(File file: files) {
-			if(file.isDirectory()) {
-				zipDir(outputStream, file, path)
-			}
-			else {
-				zipFile(outputStream, file, path)
-			}
-		}
-	}
-	
-	private void zipFile(ZipOutputStream outputStream, File file, String path) {
-		byte[] buffer = new byte[1024]
-		FileInputStream fis = new FileInputStream(file)
-		
-		outputStream.putNextEntry(new ZipEntry(buildPath(path, file.name)))
-						
-		int length
-		while ((length = fis.read(buffer)) > 0) {
-			outputStream.write(buffer, 0, length)
-		}
-		outputStream.closeEntry();
-		fis.close();
-	}
-	
-	private String buildPath(String path, String fileName) {
-		if(path == null || path.isEmpty()) {
-			return fileName
-		}
-		else {
-			return path + "/" + fileName
-		}
-	}
-	
-	private void removeTempDownloadFolder(String path) {
-		File tempDir = new File(path)
-		boolean ret = tempDir.deleteDir()
-		
-		if(!ret) {
-			log.warn "Could not delete temporary download folder: {}", path
-		}
-	}
-	
-	private byte[] getBytesFromZipFile(String path, String zipFileName) {
-		 String fullPathToZip = path + "/" + zipFileName
-		 File zipFile = new File(fullPathToZip)
-		 return zipFile.getBytes()
 	}
 }
---
+++ src/groovy/com/intercloud/util/ZipUtilities.groovy
@@ -0,0 +1,94 @@
+package com.intercloud.util
+
+import java.io.File;
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class ZipUtilities {
+	
+	private static Logger log = LoggerFactory.getLogger(ZipUtilities.class)
+	
+	public static void zipDownloadedFolder(String directory, String zipFileName) {
+		try {
+			String zipPath = directory.substring(0, directory.lastIndexOf('/'))
+			String fullZipPath = zipPath + "/" + zipFileName
+			FileOutputStream fos = new FileOutputStream(fullZipPath);
+			ZipOutputStream zos = new ZipOutputStream(fos)
+			File srcFile = new File(directory)
+			
+			packZip(zos, srcFile)
+			
+			zos.close()
+		}
+		catch(IOException) {
+			log.error "Error creating zip file: {}", IOException
+		}
+	}
+	
+	public static void packZip(ZipOutputStream outputStream, File srcFile) {
+		File[] files = srcFile.listFiles()
+		for (File file : files) {
+			if (file.isDirectory()) {
+				zipDir(outputStream, file, "")
+			}
+			else {
+				zipFile(outputStream, file, "")
+			}
+		}
+	}
+	
+	private static void zipDir(ZipOutputStream outputStream, File dir, String path) {
+		File[] files = dir.listFiles()
+		path = buildPath(path, dir.name)
+		
+		for(File file: files) {
+			if(file.isDirectory()) {
+				zipDir(outputStream, file, path)
+			}
+			else {
+				zipFile(outputStream, file, path)
+			}
+		}
+	}
+	
+	private static void zipFile(ZipOutputStream outputStream, File file, String path) {
+		byte[] buffer = new byte[1024]
+		FileInputStream fis = new FileInputStream(file)
+		
+		outputStream.putNextEntry(new ZipEntry(buildPath(path, file.name)))
+						
+		int length
+		while ((length = fis.read(buffer)) > 0) {
+			outputStream.write(buffer, 0, length)
+		}
+		outputStream.closeEntry();
+		fis.close();
+	}
+	
+	private static String buildPath(String path, String fileName) {
+		if(path == null || path.isEmpty()) {
+			return fileName
+		}
+		else {
+			return path + "/" + fileName
+		}
+	}
+	
+	public static void removeTempDownloadFolder(String path) {
+		File tempDir = new File(path)
+		boolean ret = tempDir.deleteDir()
+		
+		if(!ret) {
+			log.warn "Could not delete temporary download folder: {}", path
+		}
+	}
+	
+	public static  byte[] getBytesFromZipFile(String path, String zipFileName) {
+		 String fullPathToZip = path + "/" + zipFileName
+		 File zipFile = new File(fullPathToZip)
+		 return zipFile.getBytes()
+	}
+}
